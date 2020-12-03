2e773a802bd4bf4c3955dcc280a1742fc755cf81
Ricardo Maya <rickmaya@gmail.com>
2020-12-02 21:43:54 -0600

    add bootstrap card to App class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './index.css'
+
+//create a todo list
 
 
+class App extends React.Component {
+  
+
+
+  render() {
+    return (
+      <div className="main">
+        <Card style={{ width: '18rem'}}>
+          <Card.Img 
+            height={110}
+            width={110}
+            variant="top" 
+            src="./image/scaling.jpg" 
+          />
+          <Card.Body>
+            <Card.Title>Initial Post</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    ) 
+  }
+}
 
 
 export default App;
